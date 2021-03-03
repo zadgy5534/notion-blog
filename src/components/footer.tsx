97884469687faf5186ec08876cec390ab340a05c
嶋崎正樹 <mshimzki@Theoph.local>
2021-03-03 20:50:03 +0900

    Change to the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,19 +3,19 @@ import ExtLink from './ext-link'
 export default () => (
   <>
     <footer>
-      <span>Deploy your own!</span>
-      <ExtLink href="https://vercel.com/import/git?s=https://github.com/ijjk/notion-blog/tree/master&env=NOTION_TOKEN,BLOG_INDEX_ID&envDescription=Required+env+values+for+deploying&envLink=https://github.com/ijjk/notion-blog%23getting-blog-index-and-token">
+      <span>Also Visit:</span>
+      <ExtLink href="https://www.medieviste.org">
         <img
-          src="https://vercel.com/button"
+          src="https://www.medieviste.org/wp/wp-content/uploads/leaf1.gif"
           height={46}
           width={132}
-          alt="deploy to Vercel button"
+          alt="Humanity Study Site"
         />
       </ExtLink>
       <span>
-        or{' '}
-        <ExtLink href="https://github.com/ijjk/notion-blog">
-          view source
+        and{' '}
+        <ExtLink href="https://medieviste.org">
+          Subscribe to the mail magazine "Silva Speculationis"
         </ExtLink>
       </span>
     </footer>
