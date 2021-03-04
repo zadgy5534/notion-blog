6f0f6a6dc302856b55df76ec307d7d5aa0de9f17
嶋崎正樹 <mshimzki@Theoph.local>
2021-03-04 18:17:21 +0900

    footer change

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,15 @@ import ExtLink from './ext-link'
 export default () => (
   <>
     <footer>
-      <span>Also Visit:</span>
+      <span>
+        <ExtLink href="https://deltographos/atom">
+          Atom Feed
+        </ExtLink>
+      </span>
+      <span>Also Visit {' '}
+        <ExtLink href="https://www.medieviste.org">
+        medieviste.org
+        </ExtLink></span>
       <ExtLink href="https://www.medieviste.org">
         <img
           src="https://www.medieviste.org/wp/wp-content/uploads/leaf1.gif"
@@ -15,7 +23,7 @@ export default () => (
       <span>
         and{' '}
         <ExtLink href="https://medieviste.org">
-          Subscribe to the mail magazine "Silva Speculationis"
+          subscribe to the mail magazine "Silva Speculationis"
         </ExtLink>
       </span>
     </footer>
