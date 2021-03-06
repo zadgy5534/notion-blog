a1be0fb9075003c7d672b8fa8d143d81bf59e222
嶋崎正樹 <mshimzki@Theoph.local>
2021-03-06 10:07:44 +0900

    getTagLink remove

--- src/lib/blog-helpers.ts
+++ src/lib/blog-helpers.ts
@@ -2,9 +2,9 @@ export const getBlogLink = (slug: string) => {
   return `/blog/${slug}`
 }
 
-export const getTagLink = (tag: string) => {
+/* export const getTagLink = (tag: string) => {
   return `/blog/tag/${encodeURIComponent(tag)}`
-}
+} */
 
 export const getDateStr = date => {
   return new Date(date).toLocaleString('en-US', {
