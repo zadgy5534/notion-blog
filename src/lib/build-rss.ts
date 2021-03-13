d2e7332238fb91bb72e92f8da9d7851101682138
Masaki Shimazaki <sxolastikos@gmail.com>
2021-03-13 13:25:12 +0900

    RSS modification

--- src/lib/build-rss.ts
+++ src/lib/build-rss.ts
@@ -61,7 +61,7 @@ function createRSS(blogPosts = []) {
 
   return `<?xml version="1.0" encoding="utf-8"?>
   <feed xmlns="http://www.w3.org/2005/Atom">
-    <title>My Blog</title>
+    <title>Deltographos</title>
     <subtitle>Blog</subtitle>
     <link href="/atom" rel="self" type="application/rss+xml"/>
     <link href="/" />
